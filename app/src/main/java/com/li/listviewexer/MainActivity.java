e6f7c31bb5b88552db3b74af81f184024363d789
lsx <lsx@163.com>
2016-07-24 10:53:40 +0800

    ListView只显示文字

--- app/src/main/java/com/li/listviewexer/MainActivity.java
+++ app/src/main/java/com/li/listviewexer/MainActivity.java
@@ -1,13 +1,24 @@
 package com.li.listviewexer;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView mListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mListView = (ListView) findViewById(R.id.activity_main_list_view);
+        String[] data = {"apple", "banana", "orange", "watermelon", "pear", "grape"
+                , "orange", "watermelon", "pear", "orange", "watermelon", "pear", "orange",
+                "watermelon", "pear"};
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1, data);
+        mListView.setAdapter(adapter);
+
     }
 }
